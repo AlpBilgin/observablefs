2f496ce1946e80aabbdb5af787767ded4ee59fdd
Alp Bilgin <alp_bilgin@hotmail.com>
2020-12-30 17:08:52 +0100

    pretty

--- test/helpers/setup.ts
+++ test/helpers/setup.ts
@@ -1,5 +1,5 @@
-import { existsSync, rmSync, writeFileSync, mkdirSync } from 'fs';
-import {teardown} from './teardown'
+import { writeFileSync, mkdirSync } from 'fs';
+import { teardown } from './teardown'
 
 export function setup(dirname, filename, content) {
     const fullpath = dirname + filename
