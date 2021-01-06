5191e570dd924f8f3d8c357195fb37d785e0d2e8
Alp Bilgin <alp_bilgin@hotmail.com>
2021-01-06 21:27:15 +0100

    fix datatype

--- src/readDirObs.ts
+++ src/readDirObs.ts
@@ -2,7 +2,7 @@ import { readdir, PathLike } from "fs";
 import { promisify } from "util";
 import { Observable, from } from "rxjs";
 
-export function readDirObs(path: PathLike, options?: any ): Observable<String[]> {
+export function readDirObs(path: PathLike, options?: any ): Observable<string[]> {
   // bindNodeCallback() somehow breaks type inference and returns and unknown observable from readdir
   // converting to promise with internal node utils preserves the typing of readdir callback
   const reaDirPromise = promisify(readdir);
